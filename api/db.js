3a9e82b353ce1143df1f6ed8ae45781986e23c02
Shmedi <eduard.tupy@hotmail.com>
2021-11-16 18:37:30 -0500

    [fix] added a ssl to allow unauthorized users to use the db

--- api/db.js
+++ api/db.js
@@ -11,7 +11,9 @@ const devConfig = {
 
 const proConfig = {
   connectionString: process.env.DATABASE_URL,
-  ssl: true,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 };
 
 const pool = new Pool(
